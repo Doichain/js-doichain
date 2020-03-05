493f781c1f73e46f327caf42402944165bc0a939
Nico Krause <nico@le-space.de>
2020-03-05 14:06:07 +0500

    added bip39 library to create mnemonic and added test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,14 @@
 import chai from 'chai'
 import {printHello} from '../index'
+import {generateMnemonic} from '../lib/generateMnemonic'
 
 describe('js-doichain', function(){
-  describe('sayHello()', function(){
-    it('printHello should return hello', function(){
-      //let result = app.sayHello();
-      chai.assert.equal(printHello(), 'hello');
-    });
-  });
 
+  describe('basic doichain functions', function(){
+    it('should create a new seed phrase', function () {
+      const mnemonic = generateMnemonic()
+      chai.assert.equal(mnemonic.split(' ').length,12,'mnemonic doesnt contain 12 elements')
+    })
+  })
 
 });
