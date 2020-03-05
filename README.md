b8a5d9da80ff77bc027668ae8dad9442f65c906a
Nico Krause <nico@le-space.de>
2020-03-05 20:09:13 +0500

    added create createHdKeyFromMnemonic / validateMnemonic

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ The full Doichain protocol implementation
 
 You should use this package, if you want to request email permissions (DOI - double opt ins) from your NodeJS project (server) or even from a browser (client). You can use the library in order to add Doichain wallet functions to your mobile or web app.
 
-This repository contains functions and the documentation to create Doichain
-- hd-keys (from seed phrases)
-- addresses
+This repository contains functions and the documentation to
+- create and validate mnemonic seed phrases
+- create hd-key (from mnemonic seed phrases)
+- Doichain addresses
 - "double opt-in/DOI" request transaction
 - email verification request transactions
 - standard Doicoin transactions
--- test/test.js
+++ test/test.js
@@ -1,14 +1,31 @@
 import chai from 'chai'
 import {printHello} from '../index'
 import {generateMnemonic} from '../lib/generateMnemonic'
+import {validateMnemonic} from "../lib/validateMnemonic";
+import {createHdKeyFromMnemonic} from "../lib/createHdKeyFromMnemonic"
 
 describe('js-doichain', function(){
 
   describe('basic doichain functions', function(){
-    it('should create a new seed phrase', function () {
+    it('should create a new mnemonic seed phrase', function () {
       const mnemonic = generateMnemonic()
-      chai.assert.equal(mnemonic.split(' ').length,12,'mnemonic doesnt contain 12 elements')
+      chai.assert.equal(mnemonic.split(' ').length,12,'mnemonic doesnt contain 12 words')
     })
+
+    it('should validate a mnemonic seed phrase', function () {
+      const mnemonic = "balance blanket camp festival party robot social stairs noodle piano copy drastic"
+      const valid = validateMnemonic(mnemonic)
+      chai.assert.equal(valid,true,"mnomnic seed phrase not valid")
+    })
+
+    it('should create a hdkey from a mnemonic without password', function() {
+      const mnemonic = "balance blanket camp festival party robot social stairs noodle piano copy drastic"
+      const hdKey = createHdKeyFromMnemonic(mnemonic)
+      chai.expect(hdKey).to.have.own.property('_privateKey');
+      chai.expect(hdKey).to.have.own.property('_publicKey');
+    })
+
+
   })
 
 });
